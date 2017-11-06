888de5da9094728844ebe65b3265ea18e1ea6de0
kubac25 <jakub.cwian@gmail.com>
2017-11-06 16:51:13 +0100

    Lab 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106145358) do
+ActiveRecord::Schema.define(version: 20171106150821) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171106145358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["index"], name: "index_students_on_index", unique: true
   end
 
   create_table "students_courses", id: false, force: :cascade do |t|
