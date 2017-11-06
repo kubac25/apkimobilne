15e61e0d92c3c5a9b7372115797145d7fb6f0ffe
kubac25 <jakub.cwian@gmail.com>
2017-11-06 16:07:10 +0100

    Zabezpieczenie uzytkownikow haslem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016144800) do
+ActiveRecord::Schema.define(version: 20171106145358) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20171016144800) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "students_courses", id: false, force: :cascade do |t|
